14e1ca9a6c6fc32c23ba0bc97abf4e22682e8c52
Silas <silascosta310@gmail.com>
2025-07-20 02:58:37 -0300

    fetch data using SWR

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,16 @@
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 function Comment() {
+  const { data } = useSWR('data/data.json', fetcher);
+  console.log(data);
+
   return (
     <>
       <div>
         <div>
-          <img src="" alt="" />
+          <img alt="profile" />
           <h2></h2>
           <span></span>
           <p></p>
