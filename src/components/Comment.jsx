afee0d338e52943aea8b3b273a924c5eea2261d6
Silas <silascosta310@gmail.com>
2025-07-20 02:41:37 -0300

    feat: add initial components structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,18 @@
+function Comment() {
+  return (
+    <>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <h2></h2>
+          <span></span>
+          <p></p>
+        </div>
+        <button></button>
+        <button>Reply</button>
+      </div>
+    </>
+  );
+}
+
+export default Comment;
---
+++ src/components/CommentsSection.jsx
@@ -0,0 +1,13 @@
+import Comment from './Comment';
+
+function CommentsSection() {
+  return (
+    <>
+      <section>
+        <Comment />
+      </section>
+    </>
+  );
+}
+
+export default CommentsSection;
