77b96da5f6ab011ef7f0b06721360e0f029751af
Silas <silascosta310@gmail.com>
2025-07-30 09:23:24 -0300

    feat: add plus, minus and reply icons to buttons

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -17,10 +17,20 @@ function Comment({ profileImage, username, comment, time, likes, replies }) {
             <p className="text-grey-500">{comment}</p>
           </div>
           <div className="flex justify-between items-center mt-4">
-            <button className="text-purple-600 font-bold bg-grey-100 px-4 rounded-md">
-              {likes}
+            <div className="flex items-center gap-3 bg-grey-50 px-3.5 py-1.5 rounded-xl">
+              <button>
+                <img src="/images/icons/icon-plus.svg" alt="icon plus" />
+              </button>
+              <span className="text-purple-600 font-bold">{likes}</span>
+              <button>
+                <img src="/images/icons/icon-minus.svg" alt="icon minus" />
+              </button>
+            </div>
+
+            <button className="flex items-center gap-2 text-purple-600 font-bold">
+              <img src="/images/icons/icon-reply.svg" alt="icon reply" />
+              Reply
             </button>
-            <button className="text-purple-600 font-bold">Reply</button>
           </div>
         </div>
 
