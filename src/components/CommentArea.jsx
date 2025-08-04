458271af3831f9daa0139b8e2427349e8a5e47d6
Silas <silascosta310@gmail.com>
2025-08-04 09:17:48 -0300

    stytle: add textarea initial style and spacing structure

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,9 +1,12 @@
 function CommentArea() {
   return (
-    <div>
-      <textarea name="comment" id="comment">
-        Add a comment...
-      </textarea>
+    <div className="bg-white rounded-lg w-full p-4">
+      <textarea
+        name="comment"
+        id="comment"
+        className="w-full outline-grey-100 outline-1 resize-none text-grey-800 rounded-md px-4 py-2 h-1/2 pb-6"
+        placeholder="Add a comment..."
+      ></textarea>
       <img src="" alt="" />
       <button>Send</button>
     </div>
