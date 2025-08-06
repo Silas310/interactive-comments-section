bd4a48261f7c490354d279413af39e2d1d43962f
Silas <silascosta310@gmail.com>
2025-08-06 18:54:18 -0300

    style: refactor img embedding with svg and add color change on hover

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -26,16 +26,34 @@ function Comment({ profileImage, username, comment, time, likes, replies }) {
           </div>
           <div className="flex justify-between items-center mt-4">
             <div className="flex items-center gap-2 bg-grey-50 px-2 py-1.5 rounded-xl">
-              <button className="cursor-pointer p-2">
-                <img src="/images/icons/icon-plus.svg" alt="icon plus" />
+              <button className="cursor-pointer p-2 group">
+                <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    className="group-hover:fill-purple-600"
+                    d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 
+                    .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 
+                    0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 
+                    0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 
+                    .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+                    fill="#C5C6EF"
+                  />
+                </svg>
               </button>
               <span className="text-purple-600 font-bold">{likes}</span>
-              <button className="cursor-pointer p-2">
-                <img src="/images/icons/icon-minus.svg" alt="icon minus" />
+              <button className="cursor-pointer p-2 group">
+                <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    className="group-hover:fill-purple-600"
+                    d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 
+                    0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 
+                    .53.167h8.495Z"
+                    fill="#C5C6EF"
+                  />
+                </svg>
               </button>
             </div>
 
-            <button className="flex items-center gap-2 text-purple-600 font-bold cursor-pointer">
+            <button className="flex items-center gap-2 text-purple-600 hover:opacity-50 font-bold cursor-pointer">
               <img src="/images/icons/icon-reply.svg" alt="icon reply" />
               Reply
             </button>
