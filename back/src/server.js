281cb42e4f26723e5569bcd837701266491db5b2
Silas <silascosta310@gmail.com>
2026-02-06 19:32:29 -0300

    feat: set mongodb connection

--- back/src/server.js
+++ back/src/server.js
@@ -1,6 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
 const app = require('./app');
-const port = 3001;
 
+const url = process.env.MONGODB_URI;
+const port = process.env.PORT;
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+  });
+
+  
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
